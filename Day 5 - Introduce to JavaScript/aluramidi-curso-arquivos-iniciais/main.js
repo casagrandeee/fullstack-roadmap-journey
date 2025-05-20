0c1253e44b9efd9cc10e464e7255cfcc137a6ff9
casagrandeee <110943883+casagrandeee@users.noreply.github.com>
2025-05-20 13:03:08 -0300

    Day 5 - pt 2

--- Day 5 - Introduce to JavaScript/aluramidi-curso-arquivos-iniciais/main.js
+++ Day 5 - Introduce to JavaScript/aluramidi-curso-arquivos-iniciais/main.js
@@ -1,6 +1,33 @@
-function playSoundPom () {
-    document.querySelector('#som_tecla_pom').play();
+function playSound (audioSelector) {
+    const element = document.querySelector(audioSelector);
+
+    if (element && element.localName === 'audio') {
+            element.play();
+    } else {
+        console.log('Invalid element or selector');
+    }
 }
 
-document.querySelector('.tecla_pom').onclick = playSoundPom;
+const listOfKeys = document.querySelectorAll('.tecla');
 
+for (let counter = 0; counter < listOfKeys.length; counter = counter++) {
+    const key = listOfKeys[counter];
+    const instrument = key.classList[1];
+
+    const audioId = `#som_${instrument}`;
+
+    key.onclick = function () {
+        playSound(audioId);
+    }
+
+    key.onkeydown = function (event){
+
+        if (event.code === 'Space' || event.code ==='Enter') {
+            key.classList.add('ativa');
+        }
+    }
+
+    key.onkeyup = function (){
+        key.classList.remove('ativa');
+    }
+} 
