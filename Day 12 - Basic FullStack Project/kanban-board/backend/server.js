483611f099a9e812f99987010609409de330bd6d
casagrandeee <110943883+casagrandeee@users.noreply.github.com>
2025-06-20 19:08:04 +0100

    Day 14 - add dotenv for deploy

--- Day 12 - Basic FullStack Project/kanban-board/backend/server.js
+++ Day 12 - Basic FullStack Project/kanban-board/backend/server.js
@@ -1,7 +1,9 @@
+require('dotenv').config();
+
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const PORT = 3010;
+const PORT = process.env.PORT || 3010;
 
 app.use(cors());
 app.use(express.json());
